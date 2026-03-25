3d047a1b4fee867f54caee8dbddb372cef1b62bd
Lam Do <do_k2@denison.edu>
2026-03-25 17:32:41 -0400

    Complete Part 1

--- client.py
+++ client.py
@@ -1,13 +1,26 @@
+"""
+client.py
+Authors: 
+Description:
+"""
+
 import socket
 import ssl
 import json
 
-HOST = "127.0.0.1"
+HOST = "localhost"
 PORT = 8443
 
-with socket.create_connection((HOST, PORT)) as sock:
-    request = {"command": "GET_TIME"}
-    sock.sendall(json.dumps(request).encode())
+# InitializeT TLS Client context object
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+# Trust server certificate
+context.load_verify_locations('server.crt')
 
-    response = sock.recv(4096)
-    print("Server Response:", response.decode())
+with socket.create_connection((HOST, PORT)) as sock:
+    # Wrap socket with TLS Context
+    with context.wrap_socket(sock, server_hostname=HOST) as ssock:
+        request = {"command": "GET_TIME"}
+        ssock.sendall(json.dumps(request).encode())
+
+        response = ssock.recv(4096)
+        print("Server Response:", response.decode())
--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+"""
+server.py
+Authors:
+Description: 
+"""
+
 import socket
 import ssl
 import json
@@ -6,6 +12,12 @@ import logging
 
 HOST = "127.0.0.1"
 PORT = 8443
+logging.basicConfig(filename="TLS_log.txt", level=logging.INFO)
+
+# Initialize TLS Server context object
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# Load certificate and private key to TLS Server Context object
+context.load_cert_chain(certfile="server.crt", keyfile="server.key")
 
 # Bind TCP Socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
@@ -13,16 +25,23 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.listen(5)
     print(f"Secure server listening on {PORT}")
 
-    conn, addr = sock.accept()
-    print("Connection from", addr)
+    # Wrap socket with TLS Context, perform handshake on connect
+    with context.wrap_socket(sock, server_side=True, do_handshake_on_connect=True) as ssock:
+        while True: 
+            conn, addr = ssock.accept()
+            print("Connection from", addr)
+            # Log negotitated TLS version and cipher suite
+            logging.info(f"Connection from {addr}")
+            logging.info(f"TLS version: {conn.version()}")
+            logging.info(f"Cipher suite: {conn.cipher()}")
 
-    data = conn.recv(4096)
-    request = json.loads(data.decode())
+            data = conn.recv(4096)
+            request = json.loads(data.decode())
 
-    if request.get("command") == "GET_TIME":
-        response = {"time": datetime.datetime.now(
-            datetime.timezone.utc).isoformat()}
-    else:
-        response = {"error": "Unknown command"}
+            if request.get("command") == "GET_TIME":
+                response = {"time": datetime.datetime.now(
+                    datetime.timezone.utc).isoformat()}
+            else:
+                response = {"error": "Unknown command"}
 
-    conn.sendall(json.dumps(response).encode())
+            conn.sendall(json.dumps(response).encode())
